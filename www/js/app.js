7be7ea415688a727194e20de41d289d32a937ac0
Brian Berg <dachsund@dslextreme.com>
2013-10-23 19:39:08 -0700

    updated to handle json
    Added json file and ability to read it.

--- www/js/app.js
+++ www/js/app.js
@@ -13,21 +13,15 @@ App.FingertipRoute = Ember.Route.extend({
 	}
 });
 
-var sups = [{
-	id: '1',
-	district_name: "Adelanto School District",
-	sups_name_title: "Dr. Lily Matos DeBlieux"
-}, {
-	id: '2',
-	district_name: "Alta Loma School District",
-	sups_name_title: "Mr. Michael Whisenand"
-}, {	
-	id: '3',
-	district_name: "Apple Valley Unified School District",
-	sups_name_title: "Mr. Thomas Hoegerman"
-}, {
-	id: '4',
-	district_name: "Baker Valley Unified School District",
-	sups_name_title: "Ms. Ronda Tremblay"
-}];
-
+var sups = $.getJSON( "js/superintendents.json", function() {
+  console.log( "success" );
+})
+  .done(function() {
+    console.log( "second success" );
+  })
+  .fail(function() {
+    console.log( "error" );
+  })
+  .always(function() {
+    console.log( "complete" );
+  });
