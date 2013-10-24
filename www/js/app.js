4ef9c842685aa0a9c8319741ddfc4c2c9df5b4cd
Brian Berg <dachsund@dslextreme.com>
2013-10-23 18:49:40 -0700

    Update to Ember.js
    This update is to convert the app from just basic

--- www/js/app.js
+++ www/js/app.js
@@ -1,15 +1,33 @@
-var theScroll;
-function scroll(){
-    theScroll = new iScroll('wrapper');
-}
-document.addEventListener('DOMContentLoaded', scroll, false);
+App = Ember.Application.create();
 
-function page(toPage) {
-    var toPage = $(toPage),
-    fromPage = $("#pages .current");
-    toPage.addClass("current fade in").one("webkitAnimationEnd", function(){
-        fromPage.removeClass("current fade out");
-        toPage.removeClass("fade in")
-    });
-    fromPage.addClass("fade out");
-}
+App.Router.map(function() {
+	this.resource('home');
+	this.resource('fingertip');
+	this.resource('search');
+	this.resource('saved');
+});
+
+App.FingertipRoute = Ember.Route.extend({
+	model: function() {
+		return sups;
+	}
+});
+
+var sups = [{
+	id: '1',
+	district_name: "Adelanto School District",
+	sups_name_title: "Dr. Lily Matos DeBlieux"
+}, {
+	id: '2',
+	district_name: "Alta Loma School District",
+	sups_name_title: "Mr. Michael Whisenand"
+}, {	
+	id: '3',
+	district_name: "Apple Valley Unified School District",
+	sups_name_title: "Mr. Thomas Hoegerman"
+}, {
+	id: '4',
+	district_name: "Baker Valley Unified School District",
+	sups_name_title: "Ms. Ronda Tremblay"
+}];
+
