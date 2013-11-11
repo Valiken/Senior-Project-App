862b267d8d3cdbb7a279c709f2c318ad1104469c
Brian Berg <dachsund@dslextreme.com>
2013-11-10 20:38:37 -0800

    wat
    things are progressing!

--- www/js/sups.js
+++ www/js/sups.js
@@ -1,4 +1,18 @@
-var supsData = $.ajax({
-	url: 'http://www.trademains.com/index.php/component/supscrm_contacts/?task=superintendents&format=ajax&callback=?', 
-	dataType:'jsonp'
-});
+var supsData = []; 
+var url = 'http://www.trademains.com/index.php/component/supscrm_contacts/?task=superintendents&format=ajax&callback=?';
+
+$.ajax({
+	url: url, 
+	contentType: "application/json",
+    dataType: 'jsonp',
+    success: function(json){
+    	supsData = json; 
+		var items = [];
+		$.each(json, function(i, supsData) {
+          items.push('<li data-role="list-divider">' + supsData.district_name + '</li>' + '<li>' + supsData.sups_name_title + '</li>');
+   		});  // close each()
+   		$('#supsUL').append( items.join('') );
+   		$('#supsUL').trigger("create");
+   		$('#supsUL').listview('refresh');
+    }
+})
